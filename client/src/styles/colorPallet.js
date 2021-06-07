5ea0920dc68569549b83937a895feb0bb228156a
samqchau <67344952+samqchau@users.noreply.github.com>
2021-06-07 03:04:41 -0700

    Color pallet for cards

---
+++ client/src/styles/colorPallet.js
@@ -0,0 +1,21 @@
+let colors = {
+  white: 'white',
+  red: 'red',
+  salmon: 'salmon',
+  orange: 'orange',
+  gold: 'gold',
+  yellow: 'yellow',
+  lightGreen: 'light-green',
+  green: 'green',
+  darkGreen: 'dark-green',
+  blueGreen: 'blue-green',
+  lightBlue: 'light-blue',
+  blue: 'blue',
+  navy: 'navy',
+  purple: 'purple',
+  black: 'black',
+  gray: 'gray',
+  offPurple: 'off-purple',
+};
+
+export default colors;
