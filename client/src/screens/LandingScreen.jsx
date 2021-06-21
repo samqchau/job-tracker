7cdb696f2b7c8f46e761496e59fcbfabea820a70
samqchau <67344952+samqchau@users.noreply.github.com>
2021-06-20 23:57:05 -0700

    landing page right column structure

--- client/src/screens/LandingScreen.jsx
+++ client/src/screens/LandingScreen.jsx
@@ -23,11 +23,11 @@ const LandingScreen = ({ history }) => {
           <Col className='landing-main-center' sm={12} lg={6}>
             <LoginCard />
             <div className='landing-func-container'>
-              <div className='landing-func-header'>
+              <div className='landing-section-title'>
                 Ground Control for your Job Hunt
               </div>
-              <div className='landing-func-content'>
-                JobTrackr is a free, secure, easily-accessible web app that
+              <div className='landing-section-content'>
+                JobTrackr is a free, secure, and easily accessible web app that
                 helps users track and organize applications during the job hunt.
                 Visualize the state of your job search in a single board. See
                 how many applications, interviews and offers you’ve received in
@@ -36,18 +36,21 @@ const LandingScreen = ({ history }) => {
             </div>
           </Col>
           <Col className='landing-main-left' sm={12} lg={6}>
-            <div className='landing-main-left-demo-container'></div>
-            <div className='landing-func-container'>
-              <div className='landing-func-header'>
-                Changes are automatically saved
+            <div className='landing-main-left-demo-container'>
+              <div className='landing-section-title'>
+                Drag & Drop for Quick Changes
               </div>
-              <div className='landing-func-content'>
-                JobTrackr is a free, secure, easily-accessible web app that
-                helps users track and organize applications during the job hunt.
-                Visualize the state of your job search in a single board. See
-                how many applications, interviews and offers you’ve received in
-                one view.
-              </div>
+            </div>
+
+            <div className='landing-section-title'>
+              Changes are automatically saved
+            </div>
+            <div className='landing-section-content'>
+              JobTrackr is a free, secure, easily-accessible web app that helps
+              users track and organize applications during the job hunt.
+              Visualize the state of your job search in a single board. See how
+              many applications, interviews and offers you’ve received in one
+              view.
             </div>
           </Col>
           <LoginForm />
